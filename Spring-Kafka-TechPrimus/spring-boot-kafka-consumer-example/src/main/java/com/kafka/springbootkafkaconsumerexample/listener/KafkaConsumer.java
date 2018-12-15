c6f4aea48b558988520f1f15d38e8f04218da80e
Prateek <prateek.ashtikar@mastercard.com>
2018-12-15 22:21:59 +0530

    Spring Boot + kafka - Consumer & producer

---
+++ Spring-Kafka-TechPrimus/spring-boot-kafka-consumer-example/src/main/java/com/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.kafka.springbootkafkaconsumerexample.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.kafka.springbootkafkaconsumerexample.model.User;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "KafkaExample", group = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "KafkaExample_json", group = "group_json", containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
